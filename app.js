cc9c8c709e9e6ff8f3e58bf432678cfa51c4b0f1
unknown <pranavprajapati277@gmail.com>
2022-11-07 15:39:30 +0530

    Did

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "http://https://nameless-tor-79535.herokuapp.com//auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
